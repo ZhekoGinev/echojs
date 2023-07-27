149d4c89e24811946b64c9a422fba7eacfe2a79d
ZhekoGinev <jginev@gmail.com>
2023-07-27 13:34:13 +0300

    Change index.js to echo env var from HALab docker-compose (Requires image rebuild with name echojs:2)

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 const app = require("express")();
-const appid = process.env.HOSTNAME;
+// const appid = process.env.HOSTNAME;
+const cid = process.env.CONTAINER_NAME
 
-app.get("/", (req,res) => res.send(`Response from ${appid}`))
+app.get("/", (req,res) => res.send(`Response from ${cid}`))
 
-app.listen(9999, ()=>console.log(`${appid} is listening on 9999`))
+app.listen(9999, ()=>console.log(`${cid} is listening on 9999`))
